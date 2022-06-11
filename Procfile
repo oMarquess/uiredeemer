c51e40aeb9320744bc84985fb9b9f01f1af23424
oMarquess <olufemimarquess@gmail.com>
2022-06-11 04:40:15 -1200

    Last Time

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn uiux.wsgi --log-file -
---
+++ redy_folio/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RedyFolioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'redy_folio'
